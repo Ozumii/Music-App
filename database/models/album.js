867eee3338c2629d9b3a63eb9838c9a39f1a06c5
Ozumii <xxryanbwxx@gmail.com>
2018-07-21 23:43:18 -0400

    created

--- database/models/album.js
+++ database/models/album.js
@@ -8,3 +8,22 @@
  *  image:
  *  revenue:
  */
+
+ const mongoose = require('mongoose');
+ 
+ const Schema = mongoose.Schema;
+
+ const AlbumSchema = new Schema({
+
+     title: String,
+     date: Number,
+     copiesSold: Number,
+     numberTracks: Number,
+     image: String,
+     revenue: Number
+
+ });
+
+ const Album = mongoose.model('album',AlbumSchema);
+
+ module.exports = Album;
--- database/models/artist.js
+++ database/models/artist.js
@@ -12,3 +12,27 @@
  * retired:
  * Albums:[subdocumen]
  */
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+
+    name: String,
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    website: String,
+    netWorth: Number, 
+    labelName: String,
+    retired: String,
+    Albums: [Albums]
+
+
+});
+
+const Artist = mongoose.model('artist', Artist);
+
+module.exports = Artist;
