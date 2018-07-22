75cdec02b9a32c1bafb829a5e6ef7276982b53e4
Ozumii <xxryanbwxx@gmail.com>
2018-07-22 01:22:39 -0400

    created a new query to save an artist based on input information

--- database/queries/CreateArtist.js
+++ database/queries/CreateArtist.js
@@ -6,4 +6,12 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the Artist that was created
  */
 module.exports = (artistProps) => {
+ const artist = new Artist(artistProps
+   //  name: artistProps.name,
+   //  age: artistProps.age,
+  //   yearsActive: artistProps.yearsActive,
+  //   genre: artistProps.genre
+ );
+ //console.log(artist);
+ return artist.save();
 };
